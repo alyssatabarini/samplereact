bbadf2e3d82f11e8b679a9e2e3cc3119143509b2
alyssatabarini <alyssatabarini@gmail.com>
2017-07-24 20:33:09 -0400

    Styling Updates

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
            <div className="topheader">
            <h1 className="title"> ELA 2.0 </h1>
            <div className="line1"/>  
-           <h3 className="company"> JPMORGAN CHASEE </h3>
+           <h3 className="company"> Tabarini & CO </h3>
            <div className="line1"/>  
            <p className="user"> Tabarini, Alyssa </p> 
            <div className="line1"/>  
