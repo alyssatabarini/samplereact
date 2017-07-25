de19891fa669234f83a896e170eb077ef37cde52
alyssatabarini <alyssatabarini@gmail.com>
2017-07-24 21:25:19 -0400

    Working on Filters

--- src/components/Filter/filter.js
+++ src/components/Filter/filter.js
@@ -2,11 +2,49 @@ import React from 'react';
 import './filter.css';
 
 class Filter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: 
+            filterOptions: 
+            filterOn:
+        }
 
 
+        // this.rows = //json data
+        // this.state = {
+        //     filteredDataList: this.rows
+
+        createFilterNames() {
+        return [
+            {headerName: "Obligation ID", field: "id"},
+            {headerName: "Obligation name", field: "name"},
+            {headerName: "AORO", field: "aoro"}
+             ];
+         }
+        
+        createFiltersOptions() {
+        return [
+            {headerName: "Obligation ID", field: "id"},
+            {headerName: "Obligation name", field: "name"},
+            {headerName: "AORO", field: "aoro"}
+             ];
+         }
+    }
+
+
+
+
+//make one filter component and run a loop and create mulitple filters 
     render() {
         return(
-           <div className="filterArea" /> 
+           <div className="filterArea">
+                <div className="filterArea1"> </div> 
+                <div className="filterArea2"> </div> 
+                <div className="filterArea3"> </div> 
+            </div> 
+
         )
     }
 }
